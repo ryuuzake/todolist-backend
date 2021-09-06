9c3c126758de823585b0a209444972ccd51854f2
Riza Nafis <riza.nafis@gmail.com>
2021-09-06 18:51:49 +0700

    Add L5 Swagger Config

---
+++ app/Http/Swagger/settings/swagger-v3.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     description="To Do List OpenAPI/Swagger spec",
+ *     version="1.0.0",
+ *     title="To Do List API documentation",
+ *     termsOfService="http://swagger.io/terms/",
+ *     @OA\Contact(
+ *         email="example@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
